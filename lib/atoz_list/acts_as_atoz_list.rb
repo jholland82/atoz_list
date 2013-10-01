92bb9364ce4f19eb74ad0f9c0e8a31fad1494d81
Jeffrey Holland <jeff.holland@gmail.com>
2013-09-30 19:42:12 -0500

    cleans up test files

--- lib/atoz_list/acts_as_atoz_list.rb
+++ lib/atoz_list/acts_as_atoz_list.rb
@@ -8,7 +8,6 @@ module AtozList
     module ClassMethods
       def acts_as_atoz_list(options = {})
         include AtozList::ActsAsAtozList::LocalInstanceMethods
-        
       end
     end
 
@@ -16,9 +15,23 @@ module AtozList
       def print_test_message
         "Hello world!"
       end
+
+      def create_atoz_links(items, field)
+        atoz_letters = grab_link_letters(items, field)
+        atoz_links = generate_html_links(atoz_letters)
+      end
+
+      def grab_link_letters(items, field)
+        letters = []
+        items.each do |item|
+          value = item.send("#{field}")
+          letters << value.chars.first.upcase unless letters.include? value.chars.first.upcase
+        end
+        letters
+      end
     end
 
   end
 end
 
-ActiveController::Base.send :include, AtozList::ActsAsAtozList
+ActionController::Base.send :include, AtozList::ActsAsAtozList
--- test/acts_as_atoz_list_test.rb
+++ test/acts_as_atoz_list_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
 class ActsAsAtozListTest < ActiveSupport::TestCase
-  def test_grab_link_letters
+  def setup
     Item.create(:name => "Abyss")
     Item.create(:name => "Buddy")
     Item.create(:name => "Bully")
-    Item.create(:name => "Zoo")
+    Item.create(:name => "Zoo")   
+  end
+
+  def test_grab_link_letters
     items = Item.all
     w = WelcomeController.new
     atoz = w.grab_link_letters(items, :name)
     assert_equal atoz, ["A", "B", "Z"]
+  end
+
+  def test_create_atoz_links
+    items = Item.all
+    w = WelcomeController.new
+    atoz = w.create_atoz_links(items, :name)
+    assert_equal atoz, ["A", "B", "Z"] 
   end
 end
